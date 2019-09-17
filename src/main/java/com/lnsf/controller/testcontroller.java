b990c022e76a24ee50b3d95b796502788b213dde
zhang <you@example.com>
2019-09-17 10:47:16 +0800

    第一次提交

--- src/main/java/com/lnsf/controller/testcontroller.java
+++ src/main/java/com/lnsf/controller/testcontroller.java
@@ -3,5 +3,6 @@ package com.lnsf.controller;
 public class testcontroller {
     public static void main(String[] args) {
         System.out.println("success");
+        System.out.println("hahahhahhah");
     }
 }
